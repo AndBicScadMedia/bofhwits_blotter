0ebcd11188a75c2d62f4f9cc51c0fc67696a83b4
Andrew Mauragis <andrew@mauragis.com>
2014-10-20 11:11:16 -0400

    Escape html in requestor, user, and post sections prior to display

--- bofhapp.rb
+++ bofhapp.rb
@@ -2,6 +2,7 @@
 require 'sinatra'
 require 'mysql2'
 require 'yaml'
+require 'cgi'
 # to do:
 # move mysql connection into its own function
 # add an about page
@@ -21,7 +22,13 @@ begin
   rs = con.query("SELECT * FROM bofhwits_posts")
 @bad = []
 rs.each do |row|
-  @bad << row
+  escapeRow = Hash.new
+  escapeRow['requestor'] = CGI.escapeHTML(row['requestor'])
+  escapeRow['ts'] = row['ts']
+  escapeRow['user'] = CGI.escapeHTML(row['user'])
+  escapeRow['post'] = CGI.escapeHTML(row['post'])
+
+  @bad << escapeRow
 end
 @bad.reverse!
 rescue Mysql2::Error => e
